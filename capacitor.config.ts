d8891dcfeaf9ad05c2153b2eaa225118958eb663
Ever <ever.leandros91@gmail.com>
2025-02-20 10:27:26 -0600

    test

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,11 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.dlahavana.dhavana',
-  appName: 'dhavana',
+  appId: "com.dlahavana.dhavana",
+  appName: "dhavana",
   // webDir: '.output/public',
-  webDir: 'dist',
-  bundledWebRuntime: false
+  webDir: "dist",
+  bundledWebRuntime: false,
 };
 
 export default config;
